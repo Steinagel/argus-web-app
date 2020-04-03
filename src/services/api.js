94db11188e10b7a25c080b4e8340eb12da2e79d4
Ramon de Lima Ramos <ramonlimaramos@gmail.com>
2020-04-02 21:40:56 -0300

    fix: urls list data manipulation
    Signed-off-by: Ramon de Lima Ramos <ramonlimaramos@gmail.com>

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const baseURL = 'http://localhost:5001';
 
 const api = {
   http: axios.create({ baseURL }),
-  socket: socketio(baseURL),
+  // socket: socketio(baseURL),
 };
 
 export default api;
